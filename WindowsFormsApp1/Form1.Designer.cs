1b2e9edc641d39d1a094a253cf6194f566a2f6e7
Kritika Anand <96490053+KritikaAnand@users.noreply.github.com>
2023-12-26 12:26:19 +0530

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lstCommands = new System.Windows.Forms.ListBox();
+            this.tmrSpeaking = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // lstCommands
+            // 
+            this.lstCommands.BackColor = System.Drawing.Color.RosyBrown;
+            this.lstCommands.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lstCommands.Font = new System.Drawing.Font("Sitka Banner", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstCommands.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.lstCommands.FormattingEnabled = true;
+            this.lstCommands.ItemHeight = 29;
+            this.lstCommands.Location = new System.Drawing.Point(0, 0);
+            this.lstCommands.Name = "lstCommands";
+            this.lstCommands.Size = new System.Drawing.Size(703, 359);
+            this.lstCommands.TabIndex = 0;
+            this.lstCommands.SelectedIndexChanged += new System.EventHandler(this.lstCommands_SelectedIndexChanged);
+            // 
+            // tmrSpeaking
+            // 
+            this.tmrSpeaking.Enabled = true;
+            this.tmrSpeaking.Interval = 1000;
+            this.tmrSpeaking.Tick += new System.EventHandler(this.tmrSpeaking_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(703, 359);
+            this.Controls.Add(this.lstCommands);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstCommands;
+        private System.Windows.Forms.Timer tmrSpeaking;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Recognition;
+using System.Speech.Synthesis;
+using System.IO;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        SpeechRecognitionEngine _recognizer = new SpeechRecognitionEngine();
+        SpeechSynthesizer Synthia = new SpeechSynthesizer();
+        SpeechRecognitionEngine startlistening = new SpeechRecognitionEngine();
+        Random rnd = new Random();
+        int RecTimeOut = 0;
+        DateTime TimeNow = DateTime.Now;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            _recognizer.SetInputToDefaultAudioDevice();
+            _recognizer.LoadGrammarAsync(new Grammar(new GrammarBuilder(new Choices(File.ReadAllLines(@"DefaultCommands.txt")))));
+            _recognizer.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(Default_SpeechRecognized);
+            _recognizer.SpeechDetected += new EventHandler<SpeechDetectedEventArgs>(_recohnizer_SpeechRecognized);
+            _recognizer.RecognizeAsync(RecognizeMode.Multiple);
+
+            startlistening.SetInputToDefaultAudioDevice();
+            startlistening.LoadGrammarAsync(new Grammar(new GrammarBuilder(new Choices(File.ReadAllLines(@"DefaultCommands.txt")))));
+            startlistening.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(startlistening_SpeechRecognized);
+
+        }
+        private void Default_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            int ranNum;
+            string speech = e.Result.Text;
+
+            if(speech == "Hello Synthia")
+            {
+                Synthia.SpeakAsync("Hello, I am here");
+            }
+
+            if (speech == "How are you")
+            {
+                Synthia.SpeakAsync("I am working normally");
+            }
+
+            if (speech == "What time is it")
+            {
+                Synthia.SpeakAsync(DateTime.Now.ToString("h mm tt"));
+            }
+            if (speech == "Stop talking")
+            {
+                Synthia.SpeakAsyncCancelAll();
+                ranNum = rnd.Next(1, 2);
+                if (ranNum == 1)
+                {
+                    Synthia.SpeakAsync("Yes user");
+                }
+                if (ranNum == 2)
+                {
+                    Synthia.SpeakAsync("I am sorry i will be quiet");
+                }
+            }
+            if (speech == "Stop Listening")
+            {
+                Synthia.SpeakAsync("if you need me just ask");
+                _recognizer.RecognizeAsyncCancel();
+                startlistening.RecognizeAsync(RecognizeMode.Multiple);
+            }
+
+            if (speech == "Show commands")
+            {
+                string[] commands = File.ReadAllLines(@"DefaultCommands.txt");
+                lstCommands.Items.Clear();
+                lstCommands.SelectionMode = SelectionMode.None;
+                lstCommands.Visible = true;
+                foreach (string command in commands)
+                {
+                    lstCommands.Items.Add(command);
+                }
+
+            }
+
+            if (speech == "Hide Commands")
+            {
+                lstCommands.Visible = false;
+            }
+        }
+        private void _recohnizer_SpeechRecognized(object sender, SpeechDetectedEventArgs e)
+        {
+            RecTimeOut = 0;
+        }
+
+        private void startlistening_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            string speech = e.Result.Text;
+
+            if (speech == "Wake up")
+            {
+                startlistening.RecognizeAsyncCancel();
+                Synthia.SpeakAsync("Yes, I am here");
+                _recognizer.RecognizeAsync(RecognizeMode.Multiple);
+            }
+        }
+
+        private void tmrSpeaking_Tick(object sender, EventArgs e)
+        {
+            if(RecTimeOut == 10)
+            {
+                _recognizer.RecognizeAsyncCancel();
+            }
+            else if (RecTimeOut == 11) 
+            {
+            tmrSpeaking.Stop();
+                startlistening.RecognizeAsync(RecognizeMode.Multiple);
+                RecTimeOut = 0;
+            }
+        }
+
+        private void lstCommands_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
